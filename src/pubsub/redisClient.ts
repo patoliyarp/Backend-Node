b4842a9a45e3b82d334403f3f71ef21d2cd67608
patoliyarp <patoliya.rp@gmail.com>
2026-04-09 11:36:10 +0530

    add online redis connection for bull mq and worker

--- src/pubsub/redisClient.ts
+++ src/pubsub/redisClient.ts
@@ -3,7 +3,6 @@ import { createClient } from "redis";
 // const REDIS_URL = process.env.URL_REDIS_CONN;
 const REDIS_URL = process.env.REDIS_ONLINE;
 
-console.log("redis url", REDIS_URL);
 //Create redis client
 const redisClient = createClient({ url: REDIS_URL });
 
